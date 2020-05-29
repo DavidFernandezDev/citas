9def15ddf498d3d38ac9986ab2298ec15978786c
David <lic_david_fdez@outlook.com>
2020-05-29 18:29:23 -0500

    cita component

--- App.js
+++ App.js
@@ -1,25 +1,38 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
-import { StyleSheet, Text } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import Cita from './src/components/Cita';
 
 const App = () => {
+  const [citas, setCitas] = useState([
+    { id: '1', paciente: 'Hook', propietario: 'Joan', sintomas: 'No come'},
+    { id: '2', paciente: 'Redux', propietario: 'Itzel', sintomas: 'No duerme'},
+    { id: '3', paciente: 'Native', propietario: 'Josue', sintomas: 'No canta'}
+  ]);
+
   return (
     <>
-     <Text style={styles.encabezado}>Hola mundo!</Text>
+      <View style={styles.contenedor}>
+        <Text style={styles.encabezado}>Administrador de citas</Text>
+        <FlatList
+          data={citas}
+          renderItem={({ item }) => <Cita cita={item} />}
+        />
+      </View>
     </>
   );
 };
 
 const styles = StyleSheet.create({
+  contenedor: {
+    backgroundColor: '#AA076B',
+    flex: 1,
+  },
   encabezado: {
     textAlign: 'center',
+    marginTop: 40,
+    fontWeight: 'bold',
+    fontSize: 24,
+    color: '#FFF',
   },
 });
 
